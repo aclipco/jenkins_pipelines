82b80654be814534566813371d11c7c70a7e7554
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-02-08 10:33:21 -0600

    pull repo packer version

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,1 +1,14 @@
-
+node {
+   stage("Pull Repo"){
+     echo "hello"
+    }
+   stage("Build Image"){
+     sh "packer version"
+    }
+   stage("Send Notification to slack"){
+     echo "hello"
+    }
+   stage("Pull Repo"){
+     echo "hello"
+    }
+}

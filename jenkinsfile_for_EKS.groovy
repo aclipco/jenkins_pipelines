e3a5217794a10be7482a7b542c8000953c6eedf1
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-02-08 13:59:42 -0600

    edit

--- jenkinsfile_for_EKS.groovy
+++ jenkinsfile_for_EKS.groovy
@@ -1,17 +1,24 @@
 node{
-  stage("Pull Repo"){
-    git 'https://github.com/bkarakas/terraform-iaac-eks-burak.git'
-}
-  stage("Download Terraform"){
-        sh "terraform version"
-        sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
-        sh "unzip terraform_0.12.19_linux_amd64.zip"
-        sh "./terraform version"
-}
- stage("Set Backend"){
-        sh "source setenv.sh configurations/dev/us-west-2/dev.tfvars"
-}
-  stage("stage1"){
-    echo "hello"
-}
+    stage("Pull Repo"){
+        ws ("tmp/"){
+            git 'https://github.com/farrukh90/terraform-iaac-eks-burak.git'
+        }
+    }
+    stage("Download Terraform"){
+        ws ("tmp/") {
+            sh "terraform version"
+            sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
+            sh "unzip -o terraform_0.12.19_linux_amd64.zip"
+            sh "./terraform version"}
+    }
+    stage("Set Backend"){
+        ws ("tmp/"){
+            sh "./terraform init"
+        }
+    }
+    stage("Plan"){
+        ws ("tmp/") {
+            sh "./terraform plan -var-file configurations/dev/us-west-2/dev.tfvars"
+        }
+    }
 }

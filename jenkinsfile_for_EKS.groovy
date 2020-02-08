7f1ecf5b503baf9bc7cccb18e99552f14f9cf94a
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-02-08 12:44:46 -0600

    pull repo

---
+++ jenkinsfile_for_EKS.groovy
@@ -0,0 +1,14 @@
+node{
+  stage("Pull Repo"){
+    git 'https://github.com/bkarakas/terraform-iaac-eks-burak.git'
+}
+  stage("stage1"){
+    echo "hello"
+}
+  stage("stage1"){
+    echo "hello"
+}
+  stage("stage1"){
+    echo "hello"
+}
+}

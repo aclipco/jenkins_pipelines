f929fd34c50d72c330a762a4b9cae000c63ad9e0
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-02-08 11:17:33 -0600

    slack and email are added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,9 +9,10 @@ node {
      sh "packer build --var region=${AMI_REGION} tools/jenkins_example.json"
     }
    stage("Send Notification to slack"){
-     echo "hello"
+     slackSend channel: 'nagios_alerts', message: 'ami build'
     }
    stage("Pull Repo"){
-     echo "hello"
+     mail bcc: '', body: '''Hello,
+     Your AMI has been build''', cc: '', from: '', replyTo: '', subject: 'AMI has been build', to: 'a.clipco@gmail.com'
     }
 }

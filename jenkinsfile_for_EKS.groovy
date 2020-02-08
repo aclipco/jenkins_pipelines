b2344db9bb2524825486df96b80fb8ed4b321ad0
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-02-08 13:21:52 -0600

    source command added

--- jenkinsfile_for_EKS.groovy
+++ jenkinsfile_for_EKS.groovy
@@ -2,11 +2,14 @@ node{
   stage("Pull Repo"){
     git 'https://github.com/bkarakas/terraform-iaac-eks-burak.git'
 }
-  stage("stage1"){
-    sh "terraform version"
+  stage("Download Terraform"){
+        sh "terraform version"
+        sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
+        sh "unzip terraform_0.12.19_linux_amd64.zip"
+        sh "./terraform version"
 }
-  stage("stage1"){
-    echo "hello"
+ stage("Set Backend"){
+        sh "source setenv.sh configurations/dev/us-west-2/dev.tfvars"
 }
   stage("stage1"){
     echo "hello"

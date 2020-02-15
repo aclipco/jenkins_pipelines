85211f4bfa76db03c294ff66d2fc2e01bc0acccc
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-02-14 18:33:41 -0600

    final pipeline

--- Jenkins_to_build_terraform.groovy
+++ Jenkins_to_build_terraform.groovy
@@ -1,24 +1,22 @@
 node {
 stage("pull repo"){
-      git 'git@github.com:aclipco/docker_images.git'
+      git 'https://github.com/aclipco/docker_images.git'
     }
 stage("Build Image"){
-      sh "docker build -t . "
+      sh "docker build -t terraformimages1  ."
     }
+stage("Image Tag"){
+      sh '''docker tag terraformimages1 431846644568.dkr.ecr.us-east-1.amazonaws.com/terraformimages1'''
 stage("Login to ECR"){
     sh '''$(aws ecr get-login --no-include-email --region us-east-1)'''
     }
-    stage("Image Tag"){
-sh '''docker push 431846644568.dkr.ecr.us-east-1.amazonaws.com/terraformimages1'''
-
-}
-
 stage("Push Image"){
-    sh "docker push 431846644568.dkr.ecr.us-east-1.amazonaws.com/terraformimages1"
-
+        sh "docker images"
+        sh "docker push 431846644568.dkr.ecr.us-east-1.amazonaws.com/terraformimages1"
     }
 stage("Notification"){
     sh "echo hello"
 
     }
+}
 }

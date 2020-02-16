a20433ff4f25d4410f651c5490d4617d53fe68eb
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-02-16 16:19:07 -0600

    edit

--- Jenkins_to_build_terraform_0_12_19.groovy
+++ Jenkins_to_build_terraform_0_12_19.groovy
@@ -3,7 +3,7 @@ stage("pull repo"){
       git 'https://github.com/aclipco/docker_images.git'
     }
 stage("Build Image"){
-      sh "docker build -t 431846644568.dkr.ecr.us-east-1.amazonaws.com/terraformimages1 -f Terraform_0.12.19/Dockerfile ."
+      sh "docker build -t 431846644568.dkr.ecr.us-east-1.amazonaws.com/terraformimages1:0.12.19 -f Terraform_0.12.19/Dockerfile ."
     }
 stage("Login to ECR"){
     sh '''$(aws ecr get-login --no-include-email --region us-east-1)'''
